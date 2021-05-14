856d97aae76e2a27485601740329a57b192be42c
Taisuke Tashiro <t-tashiro@atware.co.jp>
2021-05-14 11:41:12 +0900

    [Quality] Format with Google Style

--- src/main.js
+++ src/main.js
@@ -1,56 +1,60 @@
 // Response for Uptime Robot
-const http = require("http");
-http
-  .createServer(function(request, response) {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Discord bot is active now \n");
-  })
-  .listen(3000);
+const http = require('http');
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Discord bot is active now \n');
+})
+.listen(3000);
 
 // Discord bot implements
-const discord = require("discord.js");
+const discord = require('discord.js');
 const client = new discord.Client();
 
-const members = process.env.TEAM_MEMBERS.split(",");
+const members = process.env.TEAM_MEMBERS.split(',');
 Object.freeze(members);
-const jackMaster = require("./jack-master.js");
+const jackMaster = require('./jack-master.js');
 
-client.on("ready", message => {
-  console.log("bot is ready!");
-  client.user.setPresence({ activity: { name: "SAVスプリント" } });
+client.on('ready', message => {
+  console.log('bot is ready!');
+  client.user.setPresence({activity: {name: 'SAVスプリント'}});
 });
 
-client.on("message", message => {
+client.on('message', message => {
   if (!message.mentions.has(client.user)) {
     return;
   }
-  if (message.content.includes("who's first")) {
+  if (message.content.includes('who\'s first')) {
     const orderedMembers = jackMaster.whosFirst(members);
     message.channel.send(orderedMembers);
-  } else if (message.content.includes("who's facilitator")) {
+  } else if (message.content.includes('who\'s facilitator')) {
     const roles = jackMaster.whosFacilitator(members);
     message.channel.send(roles);
-  } else if (message.content.includes("who's member")) {
-    message.channel.send(members.join("\n"));
+  } else if (message.content.includes('who\'s member')) {
+    message.channel.send(members.join('\n'));
   } else {
     message.reply(
-      "What do you want to know？:\n" +
-      "`who's first`\n" +
-      "`who's facilitator`\n" +
-      "`who's member`\n"
+        'What do you want to know？:\n' +
+        '`who\'s first`\n' +
+        '`who\'s facilitator`\n' +
+        '`who\'s member`\n'
     );
   }
 });
 
 const checks = [
-  ["DISCORD_BOT_TOKEN", "please set ENV: DISCORD_BOT_TOKEN"],
-  ["TEAM_MEMBERS", "no members in the team. please set ENV: TEAM_MEMBERS in CSV format"]
+  ['DISCORD_BOT_TOKEN', 'please set ENV: DISCORD_BOT_TOKEN'],
+  ['TEAM_MEMBERS',
+    'no members in the team. please set ENV: TEAM_MEMBERS in CSV format']
 ];
 (function environmentVariablesCheck() {
   const lacksAnyMandatoryVariable = checks
-    .filter(([variableName, unsetMessage]) => (process.env[variableName] === undefined))
-    .filter(([variableName, unsetMessage]) => { console.log(unsetMessage); return true; }) // whould be nice if we can peek in javascript
-    .reduce(() => true, false);
+  .filter(([variableName, unsetMessage]) => (process.env[variableName]
+      === undefined))
+  .filter(([variableName, unsetMessage]) => {
+    console.log(unsetMessage);
+    return true;
+  }) // whould be nice if we can peek in javascript
+  .reduce(() => true, false);
   if (lacksAnyMandatoryVariable) {
     process.exit(0);
   }
