4e8310045aea102303ceadb21e6f6fd9413b96c7
Taisuke Tashiro <t-tashiro@atware.co.jp>
2021-06-28 18:55:22 +0900

    [BugFix] Remove HTTP server since we migrated from Glitch to Heroku and are no longer using Uptime Robot

--- src/main.js
+++ src/main.js
@@ -1,11 +1,3 @@
-// Response for Uptime Robot
-const http = require('http');
-http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Discord bot is active now \n');
-})
-.listen(3000);
-
 const discord = require('discord.js');
 const client = new discord.Client();
 
