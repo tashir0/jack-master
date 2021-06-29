a11fd382c4e23d442272c79431a3fa48156372fb
Taisuke Tashiro <t-tashiro@atware.co.jp>
2021-06-29 14:56:03 +0900

    [Quality] Generalize message handling

--- src/main.js
+++ src/main.js
@@ -13,40 +13,55 @@ const jackMaster = JackMaster(members)
 
 const removeMention = (message) => message.replace(/^<[^>]+>\s/, '');
 
+const commandResolver = {
+
+  order:  {
+    executor: jackMaster.order,
+    formatter: orderedMembers => orderedMembers
+      .map((member, index) => `${index + 1}: ${member.name}`)
+      .join('\n')
+  },
+
+  meeting: {
+    executor: jackMaster.assignMeetingRoles,
+    formatter: roles => `ファシリテーター: ${roles.facilitator.name ?? 'n/a'}\n` +
+        `タイム・キーパー: ${roles.timeKeeper.name ?? 'n/a'}\n` +
+        `書記: ${roles.clerical.name ?? 'n/a'}`
+  },
+
+  random: {
+    executor: jackMaster.pickOne,
+    formatter: theOne => theOne.name
+  },
+
+  members: {
+    executor: jackMaster.members,
+    formatter: members => members.map(m => m.name).join('\n')
+  }
+};
+
+
 client.on('message', message => {
-  console.log(message.content);
   if (!message.mentions.has(client.user, { ignoreEveryone: true, ignoreRoles: true })) {
     return;
   }
-  const cleanContent = removeMention(message.content);
-  if (cleanContent.startsWith('order')) {
-    const orderedMembers = jackMaster.order();
 
-    const response = orderedMembers
-    .map((member, index) => `${index + 1}: ${member.name}`)
-    .join('\n');
+  console.log(message.content);
+  const cleanContent = removeMention(message.content.trim());
+  const firstWord = cleanContent.split(' ')[0];
 
+  const command = commandResolver[firstWord];
+  if (command) {
+    const result = command.executor();
+    const response = command.formatter(result);
     message.channel.send(response);
-  } else if (cleanContent.startsWith('meeting')) {
-    const roles = jackMaster.assignMeetingRoles();
-
-    const response = `ファシリテーター: ${roles.facilitator.name ?? 'n/a'}\nタイム・キーパー: ${roles.timeKeeper.name ?? 'n/a'}\n書記: ${roles.clerical.name ?? 'n/a'}`;
-
-    message.channel.send(response);
-  } else if (cleanContent.startsWith('random')) {
-    const theOne = jackMaster.pickOne();
-    message.channel.send(theOne.name);
-  } else if (cleanContent.startsWith('members')) {
-    const members = jackMaster.members();
-    message.channel.send(members.map(m => m.name).join('\n'));
   } else {
     message.channel.send(
         'Available commands:\n' +
         '`order` Lists all team members in a random order\n' +
         '`meeting` Assigns team members to meeting roles\n' +
         '`random` Pick one member randomly\n' +
-        '`members` Lists all team members\n'
-    );
+        '`members` Lists all team members\n');
   }
 });
 
