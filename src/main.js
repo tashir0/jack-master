6f00fe35bb9d7c4c99638d9c77d0a3716fe75487
Taisuke Tashiro <t-tashiro@atware.co.jp>
2021-09-15 10:48:05 +0900

    [Feature] Shorten PR title to avoid line break

--- src/main.js
+++ src/main.js
@@ -61,10 +61,13 @@ const commandResolver = {
       const fields = Object.entries(groupedByTicket)
         .flatMap(group => {
           const [ ticketNumber, pullRequests ] = group;
-          return pullRequests.map(pr => ({
-            name: `${ticketNumber} ${pr.title}`,
-            value: `${pr.repositoryName} [PR#${pr.requestNumber}](${pr.url}) requested by ${pr.createdUser.name} star presented by ${starPresentersToCsv(pr.starPresenters)}`
-          }));
+          return pullRequests.map(pr => {
+            const shortenedTitle = pr.title.length < 28 ? pr.title : `${pr.title.substring(0, 28)}...`; // To avoid line break
+            return {
+              name: `${ticketNumber} ${shortenedTitle}`,
+              value: `${pr.repositoryName} [PR#${pr.requestNumber}](${pr.url}) requested by ${pr.createdUser.name} star presented by ${starPresentersToCsv(pr.starPresenters)}`
+            };
+          });
         });
       const result = {
         embed: {
